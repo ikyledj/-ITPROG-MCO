8fa50e55b6047eb7c39ce0280cc3144ceda4d340
miguelreano <miguelreano10@gmail.com>
2024-11-04 00:17:59 +0800

    Add reactivity to transaction history

--- inventory_management/inventory_management/inventory_management/transaction_history.php
+++ inventory_management/inventory_management/inventory_management/transaction_history.php
@@ -25,24 +25,54 @@ $result = $stmt->get_result();
     <meta charset="UTF-8">
     <title>Transaction History</title>
     <link rel="stylesheet" href="styles1.css">
+    <style>
+        table {
+            width: 100%;
+            border-collapse: separate;
+            border-spacing: 0;
+            border-radius: 15px;
+            overflow: hidden;
+        }
+        
+        table, th, td {
+            border: 1px solid black;
+        }
+        
+        th, td {
+            padding: 10px;
+            text-align: left;
+        }
+        
+        th {
+            background-color: #003E39;
+            color: white;
+        }
+        
+        td {
+            background-color: #f2f2f2;
+            color: black;
+        }
+    </style>
 </head>
 <body>
     <?php include 'partials/header.php'; ?>
     <?php include 'partials/sidebar.php'; ?>
-    <h2>Transaction History</h2>
-    <table border="1">
-        <tr>
-            <th>Item Name</th>
-            <th>Quantity Added</th>
-            <th>Transaction Date</th>
-        </tr>
-        <?php while ($row = $result->fetch_assoc()) { ?>
+    <div class="content" id="content">
+        <h2 style="text-align: center;">Transaction History</h2>
+        <table border="1">
             <tr>
-                <td><?= $row['item_name']; ?></td>
-                <td><?= $row['quantity_added']; ?></td>
-                <td><?= $row['transaction_date']; ?></td>
+                <th>Item Name</th>
+                <th>Quantity Added</th>
+                <th>Transaction Date</th>
             </tr>
-        <?php } ?>
-    </table>
+            <?php while ($row = $result->fetch_assoc()) { ?>
+                <tr>
+                    <td><?= $row['item_name']; ?></td>
+                    <td><?= $row['quantity_added']; ?></td>
+                    <td><?= $row['transaction_date']; ?></td>
+                </tr>
+            <?php } ?>
+        </table>
+    </div>
 </body>
 </html>
