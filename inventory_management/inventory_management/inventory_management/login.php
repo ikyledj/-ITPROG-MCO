513141c0b445acd742776f3a9cd316eeb4f80c84
miguelreano <miguelreano10@gmail.com>
2024-12-05 02:35:55 +0800

    Fix login page

--- inventory_management/inventory_management/inventory_management/login.php
+++ inventory_management/inventory_management/inventory_management/login.php
@@ -6,7 +6,7 @@
     <title>Inventory Management Login</title>
     <style>
         body {
-            font-family: Arial, sans-serif;
+            font-family: 'Montserrat', Arial, sans-serif;
             background-color: #f4f4f4;
             display: flex;
             justify-content: center;
@@ -14,6 +14,16 @@
             height: 100vh;
             margin: 0;
         }
+
+        body {
+            background-image: url('logo-icons/wave-haikei.svg');
+            background-size: cover; /* Covers entire page */
+            background-position: center; /* Centers the image */
+            background-repeat: no-repeat; /* Prevents image from repeating */
+            background-attachment: fixed; /* Keeps image fixed while scrolling */
+            transition: background-color 0.4s ease, color 0.4s ease, filter 0.4s ease;
+        }
+
         .login-box {
             background-color: white;
             padding: 20px;
@@ -25,6 +35,7 @@
             text-align: center;
         }
         .login-box input[type="text"], .login-box input[type="password"] {
+            font-family: 'Montserrat', Arial, sans-serif;
             width: 100%;
             padding: 10px;
             margin: 10px 0px;
@@ -35,7 +46,7 @@
         .login-box input[type="submit"] {
             width: 100%;
             padding: 10px;
-            background-color: #28a745;
+            background-color: #002e20;
             color: white;
             border: none;
             border-radius: 5px;
@@ -43,15 +54,39 @@
             box-sizing: border-box;
         }
         .login-box input[type="submit"]:hover {
-            background-color: #218838;
+            background-color: #305a4c;
         }
         .error-message {
+            padding-top: 20px;
             color: red;
             text-align: center;
         }
+
+        .header {
+            position: fixed;
+            top: 0;
+            left: 0;
+            right: 0;
+            height: 82px;
+            background-color: #f4f4f4;
+            color: #002e20;
+            padding: 0 20px;
+            display: flex;
+            align-items: center;
+            z-index: 1000;
+            transition: color 0.4s ease, background-color 0.4s ease;
+        }
+
+        .header h1 {
+            margin: 0;
+            font-size: 24px;
+            color: #002e20;
+        }
+        
     </style>
 </head>
 <body>
+    <?php include 'partials/header.php'; ?>
     <div class="login-box">
         <h2>Login</h2>
         <form action="login_process.php" method="POST">
